bcb40aa7ecdf5c134f07fdd431b35f5a18cd78ad
gandradiego <diegogandra21@gmail.com>
2021-05-01 22:17:57 -0300

    Starting project.

---
+++ saleswebservice/src/test/java/com/mycompany/saleswebservice/SaleswebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.saleswebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaleswebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
